d86253350032aafcf45d5db7bf5d7ab60424a249
bbdav <bbdav@10.10.2.161>
2022-08-25 15:52:34 -0400

    First commit

---
+++ Assignment_0.1_GenericSmallbag_BBD/src/Item/Item.java
@@ -0,0 +1,13 @@
+package Item;
+
+public class Item<T> {
+private T e;
+
+public void setE(T e) {
+	this.e=e;
+}//end setE
+
+public T getE() {
+	return e;
+}//end getE
+}
---
+++ Assignment_0.1_GenericSmallbag_BBD/src/Item/SmallBag.java
@@ -0,0 +1,13 @@
+package Item;
+
+public class SmallBag<T> {
+private T item;
+
+public void setItem(T item) {
+	this.item=item;
+}//end setItem
+
+public T getItem() {
+	return item;
+}//end getItem
+}
